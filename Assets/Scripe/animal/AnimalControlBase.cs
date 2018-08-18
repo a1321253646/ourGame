af6ea6ade3a6fd6aeec259c649d94d24901cf4da
1321253646@qq.com <1321253646@qq.com>
2018-08-18 10:41:53 +0800

    添加动画实现机制，发现没同步代码，先存一哈

---
+++ Assets/Scripe/animal/AnimalControlBase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalControlBase
+{
+    public void set
+
+    public void setStatus() {
+
+    }
+
+    public void update() {
+
+    }
+}
