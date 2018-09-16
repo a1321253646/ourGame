69d4d9909418ebcccda7bbee738e2cf0e7727545
1321253646@qq.com <1321253646@qq.com>
2018-09-16 18:53:59 +0800

    不知道修改了啥

--- Assets/CardItemControl.cs
+++ Assets/CardItemControl.cs
@@ -6,6 +6,22 @@ public class CardItemControl : MonoBehaviour {
     CardManager mManager;
     long mCardId;
     SkillJsonBean mSkillJson;
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        float x = gameObject.GetComponent<RectTransform>().rect.width* gameObject.transform.localScale.x;
+
+        float y = gameObject.GetComponent<RectTransform>().rect.height * gameObject.transform.localScale.y;
+        Transform ob = transform.GetChild(0).GetChild(0);
+        float x1 = ob.gameObject.GetComponent<RectTransform>().rect.width;
+        float y1 = ob.gameObject.gameObject.GetComponent<RectTransform>().rect.height;
+        Debug.Log("x= " + x + " y= " + y + " x1= " + x1 + " y1=" + y1 + " Bilix=" + (x / x1) + " Biliy=" + (y / y1));
+        ob.localScale = new Vector3(x / x1, y / y1, 1);
+    }
 
     public void init(CardManager manage, long cardId) {
 
---
+++ Assets/UserCardListControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UserCardListControl : MonoBehaviour {
+    public int LinCount = 4;
+
+    public GameObject cardObject;
+
+    private GridLayoutGroup mGrilLayout;
+    CardItemControl[] mCardControl;
+    private int mGoodUiCount;
+    // Use this for initialization
+    void Start () {
+        mGrilLayout = GetComponent<GridLayoutGroup>();
+    }
+
+    // Update is called once per frame
+    float time = 0;
+    int count = 0;
+	void Update () {
+        time += Time.deltaTime;
+        if (time > 1) {
+            count++;
+            time = time - 1;
+            addCard();
+        }
+    }
+
+    public void addCard() {
+        mGoodUiCount++;
+        GameObject good = GameObject.Instantiate(cardObject,
+            new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+        good.transform.parent = gameObject.transform;
+        good.transform.localScale = Vector2.one;
+        mCardControl = GetComponentsInChildren<CardItemControl>();
+        SetGridHeight();
+    }
+    private void SetGridHeight()     //每行Cell的个数
+    {
+        int line = 0;
+        if (mGoodUiCount % LinCount != 0)
+        {
+            line = 1;
+        }
+        line += mGoodUiCount / LinCount;
+        //   Debug.Log("  gridLyout line = " + line);
+        float height = line * mGrilLayout.cellSize.y;  //行数乘以Cell的高度，3.0f是微调
+        height += (line - 1) * mGrilLayout.spacing.y;     //每行之间有间隔
+        mGrilLayout.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+    }
+}
