63674d31656d1e0cb22fb070f92c91fb20141ae3
1321253646@qq.com <1321253646@qq.com>
2018-11-14 11:32:22 +0800

    提交新表

--- Assets/Scripe/Attack/PlayControl.cs
+++ Assets/Scripe/Attack/PlayControl.cs
@@ -23,7 +23,7 @@ public class PlayControl : Attacker
         upLunhui();
         initEquip();
         mFightManager.registerAttacker (this);
-        if (InventoryHalper.getIntance().getInventorys().Count == 0 
+       /* if (InventoryHalper.getIntance().getInventorys().Count == 0 
             && InventoryHalper.getIntance().getUsercard().Count == 0
             && !GameManager.getIntance().isAddGoodForTest)
         {
@@ -49,7 +49,7 @@ public class PlayControl : Attacker
             BackpackManager.getIntance().addGoods(2110001, 1);
             BackpackManager.getIntance().addGoods(2110001, 1);
             BackpackManager.getIntance().addGoods(2110001, 1);
-        }
+        }*/
 
     }
     private void initAnimalEvent() {
