be7a6eb72c80dccb7e99013e5b90603bcc65c38d
1321253646@qq.com <1321253646@qq.com>
2019-06-27 10:34:07 +0800

    添加苹果实现类

---
+++ Assets/Scripe/IOSInAppControl.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Purchasing;
+
+public class PurchaseManager : MonoBehaviour, IStoreListener
+{
+    public BuyManager buyManager;
+    private IStoreController controller;
+
+    void Start()
+    {
+        var module = StandardPurchasingModule.Instance();
+        ConfigurationBuilder builder = ConfigurationBuilder.Instance(module);
+        builder.AddProduct("你设置的产品ID", ProductType.NonConsumable);
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+    /// <summary>
+    /// Called when Unity IAP is ready to make purchases.
+    /// </summary>
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        this.controller = controller;
+    }
+
+    /// <summary>
+    /// Called when Unity IAP encounters an unrecoverable initialization error.
+    ///
+    /// Note that this will not be called if Internet is unavailable; Unity IAP
+    /// will attempt initialization until it becomes available.
+    /// </summary>
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+    }
+
+    /// <summary>
+    /// Called when a purchase completes.
+    ///
+    /// May be called at any time after OnInitialized().
+    /// </summary>
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e)
+    {
+        return PurchaseProcessingResult.Complete;
+    }
+
+    /// <summary>
+    /// Called when a purchase fails.
+    /// </summary>
+    public void OnPurchaseFailed(Product item, PurchaseFailureReason r)
+    {
+    }
+}
