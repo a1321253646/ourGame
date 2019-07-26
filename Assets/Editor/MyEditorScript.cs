bc0865aa7955ac8b42bc1ef32a1d510526a7ef7b
1321253646@qq.com <1321253646@qq.com>
2019-07-27 01:00:46 +0800

    不知道改了什么

--- Assets/Editor/MyEditorScript.cs
+++ Assets/Editor/MyEditorScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System;
+using UnityEditor.Build.Reporting;
 
 public class MyEditorScript : Editor
 {
@@ -91,12 +92,15 @@ public class MyEditorScript : Editor
         // 打包出 APK 名
         string apkName = string.Format("{0}.apk", buildConfig.apkName);
         // 执行打包
-        string res = BuildPipeline.BuildPlayer(levels.ToArray(), apkName, buildTarget, BuildOptions.None);
+        BuildReport res = BuildPipeline.BuildPlayer(levels.ToArray(), apkName, buildTarget, BuildOptions.None);
 
-        if (res.Length > 0)
-        {
-            throw new Exception("BuildPlayer failure: " + res);
-        }
+        Debug.Log("开始打包 : " + res);
+
+     //   if (res.Length > 0)
+      //  {
+         //   throw new Exception("BuildPlayer failure: " + res);
+
+      //  }
 
         AssetDatabase.Refresh();
     }
