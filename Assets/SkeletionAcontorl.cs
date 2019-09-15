313b26940fe970dc472c9804d70d23c775230d17
1321253646@qq.com <1321253646@qq.com>
2019-09-15 14:48:40 +0800

    添加骨骼动画

---
+++ Assets/SkeletionAcontorl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletionAcontorl : MonoBehaviour
+{
+    SkeletonAnimation mAnimal;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnimal = GetComponent<SkeletonAnimation>();
+        mAnimal.state.Event += state_Event;
+
+        mAnimal.AnimationName = "walk01";
+       
+    }
+
+    void state_Event(Spine.AnimationState state, int trackIndex, Spine.Event e)
+    {
+        Debug.Log("state_Event Name=" + e.Data.Name+ " trackIndex="+ trackIndex+ " e ="+ e.String);
+
+    }
+        void Update()
+    {
+        
+    }
+}
